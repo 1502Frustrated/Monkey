af4f713aa7a7248ed0aed40fa57579422bbd0633
liuyu <791512107@qq.com>
2019-10-22 09:48:01 +0800

    maven

---
+++ Monkey1/src/main/java/com/xd/bean/Skills.java
@@ -0,0 +1,10 @@
+package com.xd.bean;
+
+/**
+ * @author 刘宇
+ * @create 2019-10-22 9:33
+ */
+public class Skills {
+    private Integer skillId;
+    private String skillEvaluate;
+}
--- Monkey1/src/main/java/com/xd/bean/Users.java
+++ Monkey1/src/main/java/com/xd/bean/Users.java
@@ -5,60 +5,60 @@ package com.xd.bean;
  * @create 2019-10-21 15:51
  */
 public class Users {
-    private Integer user_id;
-    private String user_name;
-    private String user_email;
-    private String user_password;
-
-    public Users(Integer user_id, String user_name, String user_email, String user_password) {
-        this.user_id = user_id;
-        this.user_name = user_name;
-        this.user_email = user_email;
-        this.user_password = user_password;
-    }
+    private Integer userId;
+    private String userName;
+    private String userEmail;
+    private String userPassword;
 
     public Users() {
     }
 
-    public Integer getUser_id() {
-        return user_id;
+    public Users(Integer userId, String userName, String userEmail, String userPassword) {
+        this.userId = userId;
+        this.userName = userName;
+        this.userEmail = userEmail;
+        this.userPassword = userPassword;
     }
 
-    public void setUser_id(Integer user_id) {
-        this.user_id = user_id;
+    public Integer getUserId() {
+        return userId;
     }
 
-    public String getUser_name() {
-        return user_name;
+    public void setUserId(Integer userId) {
+        this.userId = userId;
     }
 
-    public void setUser_name(String user_name) {
-        this.user_name = user_name;
+    public String getUserName() {
+        return userName;
     }
 
-    public String getUser_email() {
-        return user_email;
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
 
-    public void setUser_email(String user_email) {
-        this.user_email = user_email;
+    public String getUserEmail() {
+        return userEmail;
     }
 
-    public String getUser_password() {
-        return user_password;
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
     }
 
-    public void setUser_password(String user_password) {
-        this.user_password = user_password;
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
     }
 
     @Override
     public String toString() {
         return "Users{" +
-                "user_id=" + user_id +
-                ", user_name='" + user_name + '\'' +
-                ", user_email='" + user_email + '\'' +
-                ", user_password='" + user_password + '\'' +
+                "userId=" + userId +
+                ", userName='" + userName + '\'' +
+                ", userEmail='" + userEmail + '\'' +
+                ", userPassword='" + userPassword + '\'' +
                 '}';
     }
 }
